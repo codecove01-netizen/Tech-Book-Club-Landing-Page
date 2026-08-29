502a7b92024dd80ec5fbca30777f42f1bc56a745
codecove <codecove01@gmail.com>
2026-08-29 21:28:51 +0530

    fix: reset toast timer on repeated clicks

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const subscribeButtons = document.querySelectorAll(".subscribe-now-button");
 const toast = document.querySelector(".toast");
 
+let toastTimer;
+
 subscribeButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
         event.preventDefault();
 
+        clearTimeout(toastTimer);
         toast.classList.add("show");
 
-        setTimeout(() => {
+        toastTimer = setTimeout(() => {
             toast.classList.remove("show");
         }, 3000);
     });
